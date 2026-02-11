f66a68ce2c23744216a78ef9b5314369e6be751f
alina hashmi <heybbyali@gmail.com>
2026-02-11 13:25:07 +0530

    Enhance amicable number program with comments and messages
    Added comments explaining amicable numbers and improved output messages.

--- amicable question.py
+++ amicable question.py
@@ -1,3 +1,8 @@
+#Amicable numbers are a pair of numbers where:
+#the sum of proper divisors of the first number = the second number
+#and the sum of proper divisors of the second number = the first number.
+#Exampe- 220 and 284.
+
 n1 = int(input("enter first no: "))
 n2 = int(input("enter second no: "))
 s1 = 0
@@ -9,6 +14,7 @@ for i in range(1,n2//2+1):
     if n2 % i == 0:
         s2 += i
 if s1==n2 and s2==n1:
-    print("amicable")
+    print("Numbers are amicable")
 else:
-    print("not amicable")
+
+    print("Numbers are not amicable")
