ed348b3035a18cec13dd61cd0fd7d6e20662e398
alina hashmi <heybbyali@gmail.com>
2026-02-13 02:08:31 +0530

    Refactor prime number checking code

--- loops questions.py
+++ loops questions.py
@@ -1,11 +1,13 @@
+#to check number is prime or not using for loop
 n = int(input('Enter your number'))
 count = 0
 for i in range(1,n+1): 
     if n % i == 0:
-        count += 1
+        count += 1    #move to next number
 if count == 2:
     print(n,"is a prime number")
 else:
     print(n,"is not a prime number")
 
     
+
