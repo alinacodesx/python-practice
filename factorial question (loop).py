71b28c33b13f3958db27ce0a34d86cee7954e671
alina hashmi <heybbyali@gmail.com>
2026-02-13 02:05:39 +0530

    Refactor factorial calculation code

--- factorial question (loop).py
+++ factorial question (loop).py
@@ -1,3 +1,4 @@
+#To find factorial of a number
 n = int(input("Enter a number: "))
 if n < 0:
     print("Factorial not defined for negative numbers")
@@ -6,3 +7,4 @@ else:
     for i in range(1, n+1):
         fact *= i
     print("Factorial =", fact)
+
