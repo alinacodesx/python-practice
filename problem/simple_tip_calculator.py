a760dfa5f06e80bcb4fa745bd2cecbd12a65d42e
Alina Hashmi <heybbyali@gmail.com>
2026-03-11 17:41:56 +0530

    Add tip calculator program (Python numbers practice)

---
+++ problem/simple_tip_calculator.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter bill amount:"))
+num2 = float(input("Enter tip percentage:"))
+num2 = (num1+num2)/100
+# calculation
+total = num1 + num2
+print("Tip:",num2)
+print("Total bill:",total)
